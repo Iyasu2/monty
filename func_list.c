9f68b72e8c1f6bf3cbfff471d3197307a016b0bb
iyasu2 <iyasuforreal@gmail.com>
2023-06-21 19:52:37 +0300

    executing

---
+++ func_list.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+  * func_list - entry point
+  * @s: The instruction to be executed
+  *
+  * Return: A pointer to the function to be executed or
+  * NULL if the function doesn't exist
+  */
+void (*func_list(char *s))(stack_t **, unsigned int)
+{
+	instruction_t command[] = {
+		{ "push", push },
+		{ "pall", pall },
+		{ NULL, NULL }
+	};
+	int i = 0;
+
+	while (command[i].opcode)
+	{
+		if (strcmp(s, command[i].opcode) == 0)
+			return (command[i].f);
+
+		++i;
+	}
+
+	return (NULL);
+}
